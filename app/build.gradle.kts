9faeb4f2b06255534088823ceaab2c5a8be3bbbd
mirandaflagg <mjf846s@missouristate.edu>
2024-04-07 22:33:51 -0500

    final commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.fragment:fragment-ktx:1.4.1")
     implementation("androidx.test.ext:junit-ktx:1.1.5")
+    implementation("androidx.test.espresso:espresso-core:3.5.1")
     debugImplementation("androidx.fragment:fragment-testing:1.4.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/test/java/com/flagg/msu/criminalintent/CrimeDetailFragmentTest.kt
+++ app/src/test/java/com/flagg/msu/criminalintent/CrimeDetailFragmentTest.kt
@@ -1,13 +1,11 @@
 package com.flagg.msu.criminalintent
 
-import androidx.test.espresso.action.ViewActions.click // Chat GPT
-import androidx.test.espresso.action.ViewActions.typeText // Chat GPT
-import androidx.test.espresso.matcher.ViewMatchers.isChecked // Chat GPT
-import androidx.fragment.app.testing.launchFragmentInContainer // Chat GPT
 import androidx.fragment.app.testing.FragmentScenario
-import androidx.test.core.app.ActivityScenario.launch
-import androidx.test.espresso.Espresso.OnView
-import androidx. test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isChecked
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -26,6 +24,10 @@ class CrimeDetailFragmentTest {
         scenario = launch(CrimeDetailFragment::class.java)
     }
 
+    private fun launch(java: Class<CrimeDetailFragment>): FragmentScenario<CrimeDetailFragment> {
+        TODO("Not yet implemented")
+    }
+
     @After
     fun tearDown() {
         scenario.close()
@@ -41,3 +43,4 @@ class CrimeDetailFragmentTest {
         onView(withId(R.id.crime_title)).check(matches(withText(newTitle)))
         onView(withId(R.id.crime_solved)).check(matches(isChecked()))
     }
+}
