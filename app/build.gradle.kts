5230982dfcbbb9efceda9495a9d37d5a4739cf14
mirandaflagg <mjf846s@missouristate.edu>
2024-04-07 22:14:31 -0500

    final commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.fragment:fragment-ktx:1.4.1")
+    implementation("androidx.test.ext:junit-ktx:1.1.5")
     debugImplementation("androidx.fragment:fragment-testing:1.4.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/test/java/com/flagg/msu/criminalintent/CrimeDetailFragmentTest.kt
+++ app/src/test/java/com/flagg/msu/criminalintent/CrimeDetailFragmentTest.kt
@@ -1,14 +1,19 @@
 package com.flagg.msu.criminalintent
 
-import androidx.test.core.app.FragmentScenario
-import androidx.test.core.app.FragmentScenario.launch
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.action.ViewActions.click // Chat GPT
+import androidx.test.espresso.action.ViewActions.typeText // Chat GPT
+import androidx.test.espresso.matcher.ViewMatchers.isChecked // Chat GPT
+import androidx.fragment.app.testing.launchFragmentInContainer // Chat GPT
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.test.core.app.ActivityScenario.launch
+import androidx.test.espresso.Espresso.OnView
+import androidx. test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -25,4 +30,14 @@ class CrimeDetailFragmentTest {
     fun tearDown() {
         scenario.close()
     }
-}
+
+    @Test // Chat GPT
+    fun testFragmentScenarioOnFragment() {
+        val newTitle = "New Crime Title"
+        onView(withId(R.id.crime_title)).perform(typeText(newTitle))
+
+        onView(withId(R.id.crime_solved)).perform(click())
+
+        onView(withId(R.id.crime_title)).check(matches(withText(newTitle)))
+        onView(withId(R.id.crime_solved)).check(matches(isChecked()))
+    }
